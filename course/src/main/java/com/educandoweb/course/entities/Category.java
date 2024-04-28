e0e2b58e8e1901808cbb4041fe33b931e9c4f596
gajeel redfox <lucas.silva.cardoso1996@gmail.com>
2024-04-28 18:41:02 -0300

    product

--- course/src/main/java/com/educandoweb/course/entities/Category.java
+++ course/src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +23,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 
+	@Transient
+	private Set<Product> products = new HashSet<>();
+	
 	public Category() {
 	}
 
@@ -45,6 +51,10 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -61,5 +71,7 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 
 }
